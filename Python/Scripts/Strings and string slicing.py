636c58c6dcc24eb997fa525508fd4909b565691b
JacobNM <j@cobmart.in>
2024-05-09 15:44:50 -0400

    scripts from Python class

---
+++ Python/Scripts/Strings and string slicing.py
@@ -0,0 +1,11 @@
+#Creates a string for variable with first line
+# Extracts section of string with print line
+
+name = 'Hello! my name is Jacob Martin'
+print (name [7:])
+
+introGreeting = """Hello! My name is Jacob Martin.
+I am a student in Wayne Brown's Python class.
+The school I currently attend is Georgian College,
+and I am working towards my CSTN diploma"""
+print (introGreeting)
