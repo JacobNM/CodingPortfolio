b037af767bedced9024b2921a92b0ba15ba409ef
Jacob Martin <Jacob@gotvantage.com>
2024-08-18 16:11:49 -0400

    chore: Refactor DisplayAndDeleteFiles.sh script for Linux compatibility

--- Bash/Scripts/DisplayAndDeleteFiles.sh
+++ Bash/Scripts/DisplayAndDeleteFiles.sh
@@ -1,53 +1,26 @@
 #!/bin/bash
 
-# default option values to help determine script behaviour
-path=false
-days=false
+# Script is designed to display and delete files older than a specified number of days in a given directory
 
-
-while [ $# -gt 0 ]; do
-    case ${1} in
-        -path)
-        path=true
-        ;;
-        -days)
-        days=true
-        ;;
-        *)
-        echo """Oops. Your command is not a valid one.
-        Valid options are:
-        - path: The path to the directory you want to check for old files.
-        - days: The number of days to consider a file old. Default is 30 days."""
-        exit 1
-        ;;
-    esac
-    shift
-done
-
-# Check to see which options have been requested on command line;
-# If no options have been selected, defaults will be used
-if [[ $path == true ]]; then
-    echo "Path to directory provided."
+# Check if the user has provided the path and number of days
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <path> <days>"
+    exit 1
 fi
 
-if [[ $days == true ]]; then
-    echo "Number of days provided."
-fi
+path=$1
+days=$2
 
-# Check if path provided is a valid directory
-if [[ ! -d "$path" ]]; then
-        echo "Error: The specified path does not exist."
-        return 1
-    fi
-
-# Check if number of days is provided as an argument, otherwise use 30 days
-if [ -z "$2" ]; then
-    days=30
-else
-    days="$2"
+# Check if the specified path exists
+if [ ! -d "$path" ]; then
+    echo "Error: The specified path does not exist."
+    exit 1
 fi
 
 # Find files older than the specified number of days in the given path
+echo "Locating Files older than $days days in $path:"
+
+# Display files older than the specified number of days
 find "$path" -type f -mtime +"$days" -exec ls -ltrah {} \;
 
 # Confirmation before deletion of files
@@ -58,3 +31,4 @@ if [[ "$confirm" == [Yy]* ]]; then
 else
     echo "No files were deleted."
 fi
+
