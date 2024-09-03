c6ba9ad689b32e801ec8c8929633fb763920999f
Jacob Martin <jacob@gotvantage.com>
2024-09-03 17:19:28 -0400

    decreased indentation

--- Python/Functions/working_with_lambdas.py
+++ Python/Functions/working_with_lambdas.py
@@ -4,15 +4,15 @@ def test_lambdas_can_be_assigned_to_variables_and_called_explicitly(self):
 
     # ------------------------------------------------------------------
 
-    def make_order(self, order):
-        return lambda qty: str(qty) + " " + order + "s"
+def make_order(self, order):
+    return lambda qty: str(qty) + " " + order + "s"
 
-    def test_accessing_lambda_via_assignment(self):
-        sausages = self.make_order('sausage')
-        eggs = self.make_order('egg')
+def test_accessing_lambda_via_assignment(self):
+    sausages = self.make_order('sausage')
+    eggs = self.make_order('egg')
 
-        self.assertEqual(__, sausages(3))
-        self.assertEqual(__, eggs(2))
+    self.assertEqual(__, sausages(3))
+    self.assertEqual(__, eggs(2))
 
-    def test_accessing_lambda_without_assignment(self):
-        self.assertEqual(__, self.make_order('spam')(39823))
+def test_accessing_lambda_without_assignment(self):
+    self.assertEqual(__, self.make_order('spam')(39823))
