77a0c0928340862bdf87de307d56c25e116d41bf
Jacob Martin <jacob@gotvantage.com>
2024-09-03 15:19:59 -0400

    Add examples of list comprehension and tuple unpacking to working_with_comprehension.py

--- Python/Functions/working_with_comprehension.py
+++ Python/Functions/working_with_comprehension.py
@@ -33,3 +33,52 @@ def test_unpacking_tuples_in_list_comprehensions():
 
 # Remove hash below to test
 #test_unpacking_tuples_in_list_comprehensions()
+
+def test_double_list_comprehension():
+    list_of_eggs = ['poached egg', 'fried egg']
+    list_of_meats = ['lite spam', 'ham spam', 'fried spam']
+
+    comprehension = [ '{0} and {1}'.format(egg, meat) for egg in list_of_eggs for meat in list_of_meats]
+
+    print(f"\nThere are {(len(comprehension))} items in the comprehension list. These items are:")
+    print(f"\t- {(comprehension[0])}")
+    print(f"\t- {(comprehension[1])}")
+    print(f"\t- {(comprehension[2])}")
+    print(f"\t- {(comprehension[3])}")
+    print(f"\t- {(comprehension[4])}")
+    print(f"\t- {(comprehension[5])}")
+
+# Remove hash below to test
+#test_double_list_comprehension()
+
+def test_creating_a_set_with_set_comprehension():
+    comprehension = { x for x in 'aabbbcccc'}
+
+    print(comprehension)  # remember that set members are unique
+
+# Remove hash below to test
+#˚test_creating_a_set_with_set_comprehension()
+
+def test_creating_a_dictionary_with_dictionary_comprehension():
+    dict_of_weapons = {'first': 'fear', 'second': 'surprise',
+                        'third':'ruthless efficiency', 'fourth':'fanatical devotion',
+                        'fifth': None}
+
+    dict_comprehension = { k.upper(): weapon for k, weapon in dict_of_weapons.items() if weapon}
+
+    print(f"\nThere are {(len(dict_of_weapons))} items in the dict_of_weapons dictionary. These items are:")
+    print(f"\t- {(dict_of_weapons['first'])}")
+    print(f"\t- {(dict_of_weapons['second'])}")
+    print(f"\t- {(dict_of_weapons['third'])}")
+    print(f"\t- {(dict_of_weapons['fourth'])}")
+    print(f"\t- {(dict_of_weapons['fifth'])}")
+    
+    print(f"\nThere are {(len(dict_comprehension))} items in the dict_comprehension dictionary. These items are:")
+    print(f"\t- {(dict_comprehension['FIRST'])}")
+    print(f"\t- {(dict_comprehension['SECOND'])}")
+    print(f"\t- {(dict_comprehension['THIRD'])}")
+    print(f"\t- {(dict_comprehension['FOURTH'])}")
+    
+# Remove hash below to test
+test_creating_a_dictionary_with_dictionary_comprehension()
+
