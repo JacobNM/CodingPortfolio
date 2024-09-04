ef14a89722df1e6e50c28a9e9048a7f3ec1f8722
Jacob Martin <jacob@gotvantage.com>
2024-09-04 11:50:17 -0400

    Add lambda examples and fix indentation in working_with_lambdas.py

--- Python/Functions/working_with_lambdas.py
+++ Python/Functions/working_with_lambdas.py
@@ -1,18 +1,27 @@
-def test_lambdas_can_be_assigned_to_variables_and_called_explicitly(self):
+def test_lambdas_can_be_assigned_to_variables_and_called_explicitly():
         add_one = lambda n: n + 1
-        self.assertEqual(__, add_one(10))
+        print(add_one(10))
+
+# Remove hash from below to activate function
+#test_lambdas_can_be_assigned_to_variables_and_called_explicitly()
 
     # ------------------------------------------------------------------
 
-def make_order(self, order):
+def make_order(order):
     return lambda qty: str(qty) + " " + order + "s"
 
-def test_accessing_lambda_via_assignment(self):
-    sausages = self.make_order('sausage')
-    eggs = self.make_order('egg')
+def test_accessing_lambda_via_assignment():
+    sausages = make_order('sausage')
+    eggs = make_order('egg')
 
-    self.assertEqual(__, sausages(3))
-    self.assertEqual(__, eggs(2))
+    print(sausages(3))
+    print(eggs(2))
 
-def test_accessing_lambda_without_assignment(self):
-    self.assertEqual(__, self.make_order('spam')(39823))
+# Remove hash from below to activate function
+#test_accessing_lambda_via_assignment()
+
+def test_accessing_lambda_without_assignment():
+    print(make_order('spam')(39823))
+
+# Remove hash from below to activate function
+#test_accessing_lambda_without_assignment()
