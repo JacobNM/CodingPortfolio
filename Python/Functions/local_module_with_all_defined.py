1f08debce2c6a9a982d5842f9d819621756f0409
Jacob Martin <jacob@gotvantage.com>
2024-09-25 12:21:42 -0400

    Add local_module_with_all_defined.py with Goat, _Velociraptor, and SecretDuck classes

---
+++ Python/Functions/local_module_with_all_defined.py
@@ -0,0 +1,19 @@
+__all__ = (
+    'Goat',
+    '_Velociraptor'
+)
+
+class Goat:
+    @property
+    def name(self):
+        return "George"
+
+class _Velociraptor:
+    @property
+    def name(self):
+        return "Cuddles"
+
+class SecretDuck:
+    @property
+    def name(self):
+        return "None of your business"
