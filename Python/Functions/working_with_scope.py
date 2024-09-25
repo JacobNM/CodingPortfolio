2f36929afa4207fd9b5892acc754a7e8b0bf2bc7
Jacob Martin <jacob@gotvantage.com>
2024-09-25 11:40:14 -0400

    Refactor AboutScope class in working_with_scope.py

--- Python/Functions/working_with_scope.py
+++ Python/Functions/working_with_scope.py
@@ -10,7 +10,6 @@ import Joes_dog
 counter = 0
 
 class AboutScope():
-    pass
     #
     # NOTE:
     #   Look in Jims_dog.py and Joes_dog.py to see definitions of Dog used
@@ -49,7 +48,111 @@ class AboutScope():
         print(str)
         print(f"{AboutScope.str == str}. {AboutScope.str} is not equal to {str}")
 
-scope = AboutScope()
-# Remove hash below to activate function
+# Remove hashes below to activate function
+#scope = AboutScope()
 #scope.test_bare_bones_class_names_do_not_assume_the_current_scope()
 
+    def test_nested_string_is_not_the_same_as_the_system_string(self):
+        print(f"{self.str == type('HI')}. {self.str} is not equal to {type('HI')}")
+        
+# Remove hashes below to activate function
+#scope = AboutScope()
+#scope.test_nested_string_is_not_the_same_as_the_system_string()
+
+    def test_str_without_self_prefix_stays_in_the_global_scope(self):
+        print(f"{str == type('HI')}. {str} is equal to {type('HI')}")
+    
+# Remove hashes below to activate function
+#scope = AboutScope()
+#scope.test_str_without_self_prefix_stays_in_the_global_scope()
+
+# ------------------------------------------------------------------
+
+    PI = 3.1416
+
+    def test_constants_are_defined_with_an_initial_uppercase_letter(self):
+        print(f"{AboutScope.PI} is equal to {self.PI}")
+        print(f"{3.1416} is equal to {self.PI}")
+        
+# Remove hashes below to activate function
+#scope = AboutScope()
+#scope.test_constants_are_defined_with_an_initial_uppercase_letter()
+
+    def test_constants_are_assumed_by_convention_only(self):
+        self.PI = "rhubarb"
+        print(f"{self.PI} is equal to rhubarb")
+    
+# Remove hashes below to activate function
+#scope = AboutScope()
+#scope.test_constants_are_assumed_by_convention_only()
+
+    # ------------------------------------------------------------------
+
+    def increment_using_local_counter(self, counter):
+        counter = counter + 1
+    
+    def increment_using_global_counter(self):
+        global counter
+        counter = counter + 1
+    
+    def test_incrementing_with_local_counter(self):
+        global counter
+        start = counter
+        self.increment_using_local_counter(start)
+        print(f"{counter == start + 1}. {counter} is not equal to {start + 1}")
+    
+# Remove hashes below to activate function
+#scope = AboutScope()
+#scope.test_incrementing_with_local_counter()
+
+    def test_incrementing_with_global_counter(self):
+        global counter
+        start = counter
+        self.increment_using_global_counter()
+        print(f"{counter == start + 1}. {counter} is equal to {start + 1}")
+
+# Remove hashes below to activate function
+#scope = AboutScope()
+#scope.test_incrementing_with_global_counter()
+
+    # ------------------------------------------------------------------
+
+    def local_access(self):
+        stuff = 'eels'
+        def from_the_league():
+            stuff = 'this is a local shop for local people'
+            return stuff
+        return from_the_league()
+    
+    def nonlocal_access(self):
+        stuff = 'eels'
+        def from_the_boosh():
+            nonlocal stuff
+            return stuff
+        return from_the_boosh()
+    
+    def test_getting_something_locally(self):
+        print(self.local_access())
+    
+# Remove hashes below to activate function
+#scope = AboutScope()
+#scope.test_getting_something_locally()
+
+    def test_getting_something_nonlocally(self):
+        print(self.nonlocal_access())
+        
+# Remove hashes below to activate function
+#scope = AboutScope()
+#scope.test_getting_something_nonlocally()
+
+# ------------------------------------------------------------------
+
+    global deadly_bingo
+    deadly_bingo = ['A15', 'C90', 'G60', 'G54', 'B12', 'B6', 'B60', 'B5']
+    
+    def test_global_attributes_can_be_created_in_the_middle_of_a_class():
+        print(f"{deadly_bingo[5] == 'B6'}. {deadly_bingo[5]} is equal to B6")
+
+# Remove hashes below to activate function
+#scope = AboutScope()
+#scope.test_global_attributes_can_be_created_in_the_middle_of_a_class()
