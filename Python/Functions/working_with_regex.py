7423bfc0db431a1f61313afe84c740e8928de071
Jacob Martin <jacob@gotvantage.com>
2024-09-28 13:53:47 -0400

    First regex function

--- Python/Functions/working_with_regex.py
+++ Python/Functions/working_with_regex.py
@@ -1,2 +1,18 @@
 import re
 
+def test_matching_literal_text():
+    """
+        Lesson 1 Matching Literal String
+    """
+    string = "Hello, my name is Felix and these lessons are based " + \
+    "on Ben's book: Regular Expressions in 10 minutes."
+    regex_match = re.search('Felix', string)
+    
+    print(
+        regex_match and regex_match.group(0) and
+        regex_match.group(0) == 'Felix',
+        "I want my name")
+    # assert m and m.group(0) and m.group(0) == 'Felix', "I want my name"
+    
+# Remove hash below to activate function
+test_matching_literal_text()
