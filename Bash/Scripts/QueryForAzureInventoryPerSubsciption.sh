315df510f093508b2a747741c18565c16279b56e
Jacob <Jacob@gotvantage.com>
2025-08-15 16:40:39 -0400

    Update QueryForAzureInventoryPerSubsciption.sh to improve output file naming consistency with environment variable.

--- Bash/Scripts/QueryForAzureInventoryPerSubsciption.sh
+++ Bash/Scripts/QueryForAzureInventoryPerSubsciption.sh
@@ -1,5 +1,6 @@
 DATE=$(date +%F)
-OUT="$HOME/Downloads/azure_staging_inventory_${DATE}.csv"
+ENVIRONMENTNAME="staging"
+OUT="$HOME/Downloads/azure_${ENVIRONMENTNAME}_inventory_${DATE}.csv"
 SUBSCRIPTION="ba69753e-54c1-480b-a940-6cee4521a7ad"
 
 az account set --subscription "$SUBSCRIPTION"
